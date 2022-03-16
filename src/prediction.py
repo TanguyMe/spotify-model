b26bf5a26e6eafb85548ea0a6470193228f4d5db
TanguyMe <Tanguy.meyer67260@gmail.com>
2022-03-16 16:57:09 +0100

    Updating model

--- src/prediction.py
+++ src/prediction.py
@@ -1,20 +1,8 @@
 import pickle
 import pandas as pd
-from sklearn import metrics
 from pathlib import Path
 import os
 import json
-
-
-def cv_silhouette_scorer(estimator, X):
-    estimator.fit(X)
-    cluster_labels = estimator['model'].labels_
-    num_labels = len(set(cluster_labels))
-    num_samples = len(X.index)
-    if num_labels == 1 or num_labels == num_samples:
-        return -1
-    else:
-        return metrics.silhouette_score(X, cluster_labels)
 
 
 def prediction(input_json):
